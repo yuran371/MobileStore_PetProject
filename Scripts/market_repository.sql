2ca515503b743b2b02cde7f8cf284892e87f75b3
DmitryAnisimovJava <DmitryAnisimovJava@users.noreply.github.com>
2023-12-05 17:20:36 +0300

    Create personal_account table in PSQL.

--- Scripts/market_repository.sql
+++ Scripts/market_repository.sql
@@ -4,6 +4,13 @@ CREATE SCHEMA market;
 SHOW search_path;
 SET search_path TO market;
 
-CREATE TABLE IF NOT EXISTS customers (
-	customer_id 
-)
+CREATE TABLE IF NOT EXISTS personal_account (
+	login VARCHAR(32) PRIMARY KEY,
+	full_name VARCHAR(64) NOT NULL,
+	country VARCHAR(64) NOT NULL,
+	city VARCHAR(256) NOT NULL,
+	address VARCHAR(256) NOT NULL,
+	phone_number TEXT CONSTRAINT phone_number_constraint NOT NULL,
+	CONSTRAINT phone_number_constraint CHECK (((phone_number LIKE '+7[0-9]{10}$') AND city = 'russia') OR city != 'russia')
+);
+
