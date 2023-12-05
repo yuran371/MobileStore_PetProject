bd5222e57d9dfa64b957a7dde4f6d4081390aa1e
Artem Yuran <yuoran371@gmail.com>
2023-12-05 17:19:01 +0300

    Add script (Items)

--- Scripts/market_repository.sql
+++ Scripts/market_repository.sql
@@ -4,3 +4,12 @@ CREATE SCHEMA market;
 SHOW search_path;
 SET search_path TO market;
 
+CREATE TABLE Items (item_id BIGSERIAl PRIMARY KEY,
+					model VARCHAR(32) NOT NULL,
+					brand VARCHAR(32) NOT NULL,
+					attributes VARCHAR(128) NOT NULL,
+					price NUMERIC(12,2) CHECK (price > 0),
+					quantity INT CHECK (quantity >= 0)) NOT NULL;
+				
+DROP TABLE Items;
+					
