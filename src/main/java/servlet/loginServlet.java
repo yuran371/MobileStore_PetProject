fef6f703bc12a4248b4aa076c253f20307fa16bd
Artem Yuran <yuoran371@gmail.com>
2023-12-12 16:45:03 +0300

    Rename package service

--- src/main/java/servlet/loginServlet.java
+++ src/main/java/servlet/loginServlet.java
@@ -9,7 +9,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import services.AddLoginService;
+import service.AddLoginService;
 
 @WebServlet("/loginResult")
 public class loginServlet extends HttpServlet {
