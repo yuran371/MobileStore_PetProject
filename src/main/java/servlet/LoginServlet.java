634749a7a40c36914563ea7eef830851d039ecdd
Artem Yuran <yuoran371@gmail.com>
2023-12-14 11:27:32 +0300

    Start realization LoginServlet.doGet() and small refactor

--- src/main/java/servlet/LoginServlet.java
+++ src/main/java/servlet/LoginServlet.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.Optional;
@@ -12,16 +13,25 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.Cleanup;
 import service.PersonalAccountService;
 
 @WebServlet("/loginResult")
 public class loginServlet extends HttpServlet {
 
-	private final static String AUTORIZATION_CHECK = "loginCheck";
+	private final static String AUTHORIZATION_CHECK = "loginCheck";
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		req.getCookies();
+		Cookie[] cookies = req.getCookies();
+		@Cleanup
+		PrintWriter writer = resp.getWriter();
+		if (cookies == null || Arrays.stream(cookies).filter(cookie -> cookie.getName().equals(AUTHORIZATION_CHECK))
+				.findFirst().isEmpty()) {
+			writer.write("<h1>You are not authorized. Please go to the Registration page</h1>");
+			writer.write("<a href = \"/login.html\">Registration</a>");
+		}
+		
 	}
 
 	@Override
@@ -30,7 +40,7 @@ public class loginServlet extends HttpServlet {
 		resp.setContentType("text/html");
 		var cookies = req.getCookies();
 		try (var writer = resp.getWriter()) {
-			if (cookies == null || (Arrays.stream(cookies).map(cookie -> cookie.getName().equals(AUTORIZATION_CHECK))
+			if (cookies == null || (Arrays.stream(cookies).filter(cookie -> cookie.getName().equals(AUTHORIZATION_CHECK))
 					.findFirst().isEmpty())) {
 				var email = req.getParameter("email");
 				var name = req.getParameter("name");
@@ -51,14 +61,14 @@ public class loginServlet extends HttpServlet {
 					writer.write("<a href = \"/login.html\">Go back to login</a>");
 				}
 				if (addAccountResult.isPresent()) {
-					var cookie = new Cookie(AUTORIZATION_CHECK, addAccountResult.get().toString());
+					var cookie = new Cookie(AUTHORIZATION_CHECK, addAccountResult.get().toString());
 					cookie.setPath("/loginResult");
 					cookie.setMaxAge(60 * 60);
 					resp.addCookie(cookie);
 				}
 			} else {
 				Cookie authorizationCookie = Arrays.stream(cookies)
-						.filter(cookie -> cookie.getName().equals(AUTORIZATION_CHECK)).findFirst().get();
+						.filter(cookie -> cookie.getName().equals(AUTHORIZATION_CHECK)).findFirst().get();
 				DtoPersonalAccount byId = PersonalAccountService.getInctance()
 						.getById(Long.parseLong(authorizationCookie.getValue()));
 				writer.write("""
