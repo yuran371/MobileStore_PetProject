94046f744d766ab1bafae962900b3994a5923e46
DmitryAnisimovJava <DmitryAnisimovJava@users.noreply.github.com>
2024-04-01 20:06:12 +0300

    Add Login and Registry servlet + context.xml and JWT tokenization.

--- src/main/java/validator/UniqueEmailValidator.java
+++ src/main/java/validator/UniqueEmailValidator.java
@@ -1,23 +1,14 @@
 package validator;
 
 import dao.PersonalAccountDao;
+import jakarta.enterprise.inject.spi.CDI;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
-import org.jboss.weld.environment.se.Weld;
-import org.jboss.weld.environment.se.WeldContainer;
 
 public class UniqueEmailValidator implements ConstraintValidator<UniqueEmail, String> {
 
-    private PersonalAccountDao personalAccountDao;
+    private static final PersonalAccountDao personalAccountDao = CDI.current().select(PersonalAccountDao.class).get();
 
-    private static Weld weld = new Weld();
-    private static WeldContainer weldContainer = weld.initialize();
-
-    @Override
-    public void initialize(UniqueEmail constraintAnnotation) {
-        ConstraintValidator.super.initialize(constraintAnnotation);
-        personalAccountDao = weldContainer.select(PersonalAccountDao.class).get();
-    }
 
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
